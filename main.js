ab842eefd5cf2d6612de6597207c71708b51e8b6
Sayed <max@blineco.com>
2023-05-21 02:31:22 +1000

    Sample-Notion (In-Progress)

---
+++ main.js
@@ -0,0 +1,108 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+
+    <nav>
+        <div class="nav-items">        
+            <a href="#" title="Github">Source Code</a>
+            <!--<a href="#" title="About">About</a>-->
+            <!--<a href="#" title="Contacts">Contacts</a>-->
+        </div>
+    </nav>
+
+    
+    
+    <article>
+        <p id="heading" title="Text">A Wonderful Text</p>  
+            <div class="home-page">
+                <div class="mg-1 default-cursor" title="Reading Time">
+                    <span class="reading-length">                
+                        <i class="fa fa-clock-o" style="color: #b0b0b0; font-size: 1.2em; font-weight: 200;"></i>
+                        <span>1 min read</span>
+                    </span>
+                </div>
+
+            <div class="reading">
+                <p>
+                    A conversation with Ray Garcia, Jonathan Gold, Evan Kleiman, Bricia Lopez, 
+                    and Carlos Salgado about the evolving role of Mexican cuisine in LA as culture, 
+                    art, and craft Together, critic Jonathan Gold, food scholar Evan Kleiman, and
+                    chefs Ray Garcia, Bricia Lopez, and Carlos Salgado have spent more than a lifetime
+                    preparing, eating, and writing about Latin American cuisine. Before their recent 
+                    panel discussion at the Getty, offered in conjunction with Pacific Standard Time,
+                    they sat down with us to reflect on the role of Mexican cuisine in the life of
+                    Los Angeles and how we might expand our taste buds to go deeper.
+                </p>
+                <p>
+                    A conversation with Ray Garcia, Jonathan Gold, Evan Kleiman, Bricia Lopez, 
+                    and Carlos Salgado about the evolving role of Mexican cuisine in LA as culture, 
+                    art, and craft Together, critic Jonathan Gold, food scholar Evan Kleiman, and
+                    chefs Ray Garcia, Bricia Lopez, and Carlos Salgado have spent more than a lifetime
+                    preparing, eating, and writing about Latin American cuisine. Before their recent 
+                    panel discussion at the Getty, offered in conjunction with Pacific Standard Time,
+                    they sat down with us to reflect on the role of Mexican cuisine in the life of
+                    Los Angeles and how we might expand our taste buds to go deeper.
+                </p> 
+                <p>                
+                    The highest point in my life is being with close family and friends, 
+                    in the backyard over a fire, cooking over the course of many hours, 
+                    sitting under the sky, and just filling this space with so much warmth. 
+                    When I'm cooking there, I feel much more like an artist than when I
+                    worked in the highest-concept restaurants. They are like museums of food,
+                    accessible only with a certain level of literacy on the part of the diner,
+                    with a certain level of cultural experience and background.
+                </p>
+                <p>                
+                    The highest point in my life is being with close family and friends, 
+                    in the backyard over a fire, cooking over the course of many hours, 
+                    sitting under the sky, and just filling this space with so much warmth. 
+                    When I'm cooking there, I feel much more like an artist than when I
+                    worked in the highest-concept restaurants. They are like museums of food,
+                    accessible only with a certain level of literacy on the part of the diner,
+                    with a certain level of cultural experience and background.
+                </p>
+                <p>                
+                    The highest point in my life is being with close family and friends, 
+                    in the backyard over a fire, cooking over the course of many hours, 
+                    sitting under the sky, and just filling this space with so much warmth. 
+                    When I'm cooking there, I feel much more like an artist than when I
+                    worked in the highest-concept restaurants. They are like museums of food,
+                    accessible only with a certain level of literacy on the part of the diner,
+                    with a certain level of cultural experience and background.
+                </p>
+                <p>                
+                    The highest point in my life is being with close family and friends, 
+                    in the backyard over a fire, cooking over the course of many hours, 
+                    sitting under the sky, and just filling this space with so much warmth. 
+                    When I'm cooking there, I feel much more like an artist than when I
+                    worked in the highest-concept restaurants. They are like museums of food,
+                    accessible only with a certain level of literacy on the part of the diner,
+                    with a certain level of cultural experience and background.
+                </p>
+                <p>                
+                    The highest point in my life is being with close family and friends, 
+                    in the backyard over a fire, cooking over the course of many hours, 
+                    sitting under the sky, and just filling this space with so much warmth. 
+                    When I'm cooking there, I feel much more like an artist than when I
+                    worked in the highest-concept restaurants. They are like museums of food,
+                    accessible only with a certain level of literacy on the part of the diner,
+                    with a certain level of cultural experience and background.
+                </p>
+            </div>
+
+
+            </div>
+            </article>
+
+        <footer>
+            <span>Made with</span>
+                <i class="fa fa-heart-o"  font-size: 1em;"></i>
+            <span>by</span>
+            <a href="#">@SayedDileri</a>
+        </footer>
+
+
+
+
+
+`
